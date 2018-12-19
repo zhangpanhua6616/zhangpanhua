a7edf2a5f68ca43e788b154d093af684728a8b03
张攀华 <19825292682@139.com>
2018-12-19 22:15:09 +0800

    first

---
+++ src/main/java/com/soft1841/sm/Main.java
@@ -0,0 +1,23 @@
+package com.soft1841.sm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("登录界面");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
